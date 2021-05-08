a68fe9d91ef6ed87510e1e826094eac66ec2db78
Luis Otavio Azevedo Maciel <luismaciel@feevale.br>
2021-05-07 23:54:26 -0300

    Include project

---
+++ Demo.SendEmail/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using AspCore_Email.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspCore_Email.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
